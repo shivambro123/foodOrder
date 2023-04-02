71153cf1ad9899753b457c30080c2e7a0785df17
shivam <shi845285@gmail.com>
2023-04-02 14:40:26 +0530

    Button removed

--- src/Component/Cart/CartItem/CartItem.jsx
+++ src/Component/Cart/CartItem/CartItem.jsx
@@ -10,7 +10,7 @@ const CartItem = ({src,name,price,quantity,id}) => {
     <div className='cart-wrapper'>
         <div className='cart-image'><img src={src} alt="image" className='cart-size'/></div>
         <div className='cart-name'><h3>{name}</h3></div>
-        <div className='cart-quantity'><button onClick={()=>dispatch(decrementItem())}>-</button><span>{quantity}</span><button onClick={()=>dispatch(incrementItem())}>+</button></div>
+        <div className='cart-quantity'><span>{quantity}</span></div>
         <div className='cart-price'><span>&#8360; {price * quantity}</span></div>
         <div className='cart-remove'><Button variant="outline-dark" onClick={()=>dispatch(removeItem(id))}>Remove</Button></div>
     </div>
